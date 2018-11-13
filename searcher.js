3dd72b9b9ce50be8887738f699fefec01482f02c
67millwood <info@track2development.com>
2018-11-13 16:16:43 +0000

    completed with string literal

--- searcher.js
+++ searcher.js
@@ -2,23 +2,29 @@ const pg = require("pg");
 const settings = require("./settings"); // settings.json
 
 const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
+  "user": "development",
+  "password": "development",
+  "database": "test_db",
+  "hostname": "localhost",
+  "port": 5432,
+  "ssl": true
 });
 
+var myArg = process.argv.slice(2);
+
+function findPaul(text) {
 client.connect((err) => {
   if (err) {
     return console.error("Connection Error", err);
   }
-  client.query("SELECT $1::int AS number", ["1"], (err, result) => {
+  client.query(`SELECT * from famous_people WHERE first_name= '${text}' `, (err, result) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0].number); //output: 1
+    console.log(result.rows); //output: 1
     client.end();
   });
 });
+};
+
+findPaul(myArg);
